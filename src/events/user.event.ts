a7cef58ee867fab6c38ac873d045d630edffb179
Nick Freitas <nickfrei@gmail.com>
2021-08-22 21:18:15 -0400

    Updates

--- src/events/user.event.ts
+++ src/events/user.event.ts
@@ -3,5 +3,23 @@ import { UserEventType } from './users.event-types';
 
 export interface UserEvent {
     type: UserEventType,
-    data: User
+    data: Partial<User>
+}
+
+export interface CreateUserEvent extends UserEvent {
+    type: UserEventType.CREATE_USER,
+    data: {
+        id: string,
+        fullname: string,
+        email: string,
+        password: string,
+    }
+}
+
+export interface UpdateUserEvent extends UserEvent {
+    type: UserEventType.UPDATE_USER,
+    data: {
+        id: string,
+        fullname: string,
+    }
 }
