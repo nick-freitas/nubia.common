d5fac80521091d6d203051f76b5a93b56ebc7a79
Nick Freitas <nickfrei@gmail.com>
2021-08-23 19:31:48 -0400

    Updates

---
+++ src/errors/out-of-order-event.exception.ts
@@ -0,0 +1,16 @@
+import { EventType } from '../events';
+import { BadRequestException } from './bad-request.exception';
+
+export class OutOfOrderEventException extends BadRequestException {
+  
+  constructor(
+    eventType: EventType,
+    expectedVersion: number,
+    actualVersion: number
+  ) {
+    super(
+      `${eventType} - Expected Version ${expectedVersion} but got ${actualVersion} instead`,
+      "Out of Order Event Exception",
+    );
+  }
+}
--- src/events/index.ts
+++ src/events/index.ts
@@ -1,4 +1,9 @@
+import { GamebookEventType } from './gamebook.event-types';
+import { UserEventType } from './user.event-types';
+
 export * from './gamebook.event';
 export * from './gamebook.event-types';
 export * from './user.event';
 export * from './user.event-types';
+
+export type EventType = UserEventType | GamebookEventType
