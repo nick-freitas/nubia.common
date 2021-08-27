0db584c1e703a8db703fd288ebbfc34c274a665e
Nick Freitas <nickfrei@gmail.com>
2021-08-26 21:36:57 -0400

    Updates

--- src/events/gamebook.event-types.ts
+++ src/events/gamebook.event-types.ts
@@ -4,7 +4,4 @@ export enum GamebookEventType {
   GAMEBOOK_CREATED = "gamebook:created",
   UPDATE_GAMEBOOK = "gamebook:update",
   GAMEBOOK_UPDATED = "gamebook:updated",
-  RESET_GAMEBOOK_CHOICES = "gamebook:reset_choices",
-  UNDO_LAST_GAMEBOOK_CHOICE = "gamebook:undo_last_choice",
-  MAKE_GAMEBOOK_CHOICE = "gamebook:make_choice",
 }
--- src/events/gamebook.event.ts
+++ src/events/gamebook.event.ts
@@ -67,40 +67,6 @@ export const isGamebookUpdatedEvent = (
   e: NubiaEvent
 ): e is GamebookUpdatedEvent => e.type === GamebookEventType.GAMEBOOK_UPDATED;
 
-export interface ResetGamebookChoiceEvent extends GamebookEvent {
-  type: GamebookEventType.RESET_GAMEBOOK_CHOICES;
-  data: {
-    id: string;
-  };
-}
-export const isResetGamebookChoiceEvent = (
-  e: NubiaEvent
-): e is ResetGamebookChoiceEvent =>
-  e.type === GamebookEventType.RESET_GAMEBOOK_CHOICES;
-
-export interface UndoLastGamebookChoiceEvent extends GamebookEvent {
-  type: GamebookEventType.UNDO_LAST_GAMEBOOK_CHOICE;
-  data: {
-    id: string;
-  };
-}
-export const isUndoLastGamebookChoiceEvent = (
-  e: NubiaEvent
-): e is UndoLastGamebookChoiceEvent =>
-  e.type === GamebookEventType.UNDO_LAST_GAMEBOOK_CHOICE;
-
-export interface MakeGamebookChoiceEvent extends GamebookEvent {
-  type: GamebookEventType.MAKE_GAMEBOOK_CHOICE;
-  data: {
-    id: string;
-    progressionId: string;
-  };
-}
-export const isMakeGamebookChoiceEvent = (
-  e: NubiaEvent
-): e is MakeGamebookChoiceEvent =>
-  e.type === GamebookEventType.MAKE_GAMEBOOK_CHOICE;
-
 export interface GetGamebookEvent extends GamebookEvent {
   type: GamebookEventType.GET_GAMEBOOK;
   data: {
